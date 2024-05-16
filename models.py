9c64ba83f2783ea41e54f165fa23627680f314e4
nonelone <filip.panek@sfi.pl>
2024-05-16 23:38:16 +0200

    Made CLI more expressive, made password hidden in CLI

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 import os.path
+import getpass
 
 db = SQLAlchemy()
 
@@ -30,9 +31,8 @@ class Tokens(db.Model):
     token_id = db.Column(db.Integer, db.ForeignKey(Token.id))
 
 def add_admin(name, last_name, email, password):
-    print('adding!')
+    print('Creating new admin account...')
     if Admin.query.filter_by(email=email).first() is None:
-        print('none!')
         new_admin = Admin(admin_name=name, admin_last_name = last_name, email=email, password=generate_password_hash(password))
         db.session.add(new_admin)
         db.session.commit()
@@ -40,17 +40,20 @@ def add_admin(name, last_name, email, password):
 
 def init_database():
     if os.path.isfile('instance/db'):
-        pass
+        print("Found database...")
     else:
+        print("Database not found! Creating new database...")
         db.create_all()
 
     if Admin.query.first() is not None:
         pass
     else:
-        print("NO ADMIN FOUND\nYou need to create admin!")
+        print("No admin has been found!\nYou need to create admin!")
         name = input("Admin's name: ")
         last_name = input("Admin's last name: ")
         email = input("Admin's email: ")
-        password = input("Admin's password: ")
+        password = getpass.getpass(prompt="Admin's password: ") #Password will not be echoed
         if name and last_name and email and password:
             add_admin(name, last_name, email, password)
+
+    print("Database initialized...")
