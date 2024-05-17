d422804c3d757e715ae80e0ded8711b90e778b13
nonelone <nonelpublic@proton.me>
2024-05-17 15:45:44 +0200

    Modified pinguś, first test of verified commit

--- pinguś.py
+++ pinguś.py
@@ -1,10 +1,11 @@
 import requests
+import config
 
 while True:
     token = input("$ token > ")
     key = input("$ key card > ")
     try:
-        with requests.post('http://127.0.0.1:5000/api', data={'token' : token, 'card_id' : key}) as r:
+        with requests.post(f'http://127.0.0.1:2137/api', data={'token' : token, 'card_id' : key}) as r:
             print(r.status_code, r.reason)
             print(r.text)
 
