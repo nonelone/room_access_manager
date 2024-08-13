38a4293ffdc64d42d98728affbe0e33af921a1c9
= <=>
2024-08-13 12:34:05 +0200

    Fixing api, pt. II

--- api.py
+++ api.py
@@ -18,19 +18,10 @@ def api():
         print(token, card_id)
         if token is None or card_id is None: abort(403)
 
-        lock_id = Lock.query.filter_by(token=token).first().id
-        print(lock_id)
-        if lock_id is None: abort(403)
-
-        connected = db.session.query(Tokens).filter(Tokens.nfc_id == card_id).filter(Tokens.lock_id == lock_id).all()
-        print(connected)
-        #allowed_tokens = Tokens.query.with_entities(Tokens.token_id).all()
-        #avaliable_locks = Tokens.query.filter(Lock.id == lock_id).all()
-        #allowed_token_ids = [token[0] for token in allowed_tokens]
-        #abort(503)
+        lock = Lock.query.filter_by(token=token).first()
+        if lock is None: abort(403)
+        lock_id = lock.id
         
-        #if token not in allowed_token_ids:
-        #    abort(403)
         if check_authority(card_id, lock_id):
             return 'Authorized'
         else:
